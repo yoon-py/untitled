dc423dedf1554847bae3edb19673d607f20f88ed
yoonhwan <dbsghksdl75@naver.com>
2023-11-09 12:30:35 +0900

    python 추가

---
+++ index.py
@@ -0,0 +1,11 @@
+id = input('가입 ID')
+password = input('가입 Password')
+
+idt = input('로그인 ID')
+passwordt = input('로그인 Password')
+
+if idt == id and passwordt == password:
+
+    print('welcome')
+else:
+    print('get out')
